5fd619960f4f57a5b9e89cf9a51dd344defd4138
Vasiliy Shapovalov <8428979-VasiliyShapoval@users.noreply.replit.com>
2024-07-03 19:55:21 +0000

    Initial commit

---
+++ init_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+import sqlite3
+
+
+# export function to create a mysql in memory db and fill it with data
+def make_db_uri():
+    # URL of the file
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTLLVgx5ygrIfZ_nT0RsXV69I1j1nkfUMvztdkSXURPt2U9hMa9WCf2tgkKQoVDnsy2ZQt2w-m4rx7J/pub?output=xlsx'
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Save the content of the request as a file
+    with open('downloaded_file.xlsx', 'wb') as f:
+        f.write(response.content)
+
+    # Load the Excel file again
+    excel_file = 'downloaded_file.xlsx'
+    sheets = pd.read_excel(excel_file, sheet_name=None)
+
+    db_uri = 'file::memory:?cache=shared'
+    # Create an in-memory SQLite database and load all sheets as tables
+    conn = sqlite3.connect(db_uri)
+
+    for sheet_name, df in sheets.items():
+        df.to_sql(sheet_name, conn, index=False, if_exists='replace')
+
+    # Verify the tables loaded
+    tables = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table';",
+                         conn)
+    return db_uri
---
+++ queries.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data  # 👈 Add the caching decorator
+def prepare_data(_conn):
+  query = """
+  SELECT *
+  FROM "VehicleChassis"
+  """
+  
+  vehicle_chassis = pd.read_sql(query, _conn)
+  vehicle_chassis_dict = vehicle_chassis.set_index('Type').to_dict(orient='index')
+  
+  
+  query = """
+  SELECT *
+  FROM "GroundWeapons"
+  """
+  
+  ground_weapons_df = pd.read_sql(query, _conn)
+  ground_weapons_grouped = {size: group.set_index('Name').to_dict('index') for size, group in ground_weapons_df.groupby('Size')}
+  
+  
+  query = """
+  SELECT *
+  FROM "VehicleSlotCosts"
+  """
+  
+  vehicle_slot_costs = pd.read_sql(query, _conn)
+  vehicle_slot_costs_dict = vehicle_slot_costs.set_index('Slot Type').to_dict(orient='index')
+
+  query = """
+  SELECT *
+  FROM "VehicleSlotsConversion"
+  """
+  
+  vehicle_slots_conversion = pd.read_sql(query, _conn)
+
+  vehicle_wargear = pd.read_sql(query, _conn)
+  vehicle_wargear_dict = vehicle_wargear.set_index('Name').to_dict(orient='index')
+  
+
+  return vehicle_chassis_dict, ground_weapons_grouped,   vehicle_slot_costs_dict, vehicle_slots_conversion, vehicle_wargear_dict
+  
