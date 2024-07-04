0f476d7f5347492e92b9044e41c199883bffbb29
vshvsh <Vasiliy.Shapovalov@gmail.com>
2024-07-04 08:06:24 +0000

    fix: move pages to root

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from init_data import make_db_uri
 from queries import prepare_data
 
 st.write("navigate generators in the left sidebar")
-create_page = st.Page("pages\\create_vehicle.py", title="Create vehicle")
-refit_page = st.Page("pages\\refit_squad.py", title="Refit squad")
+create_page = st.Page("pages_create_vehicle.py", title="Create vehicle")
+refit_page = st.Page("pages_refit_squad.py", title="Refit squad")
 ng = st.navigation([create_page, refit_page])
 ng.run()
